dca5757f6fee448de8acef70f633c1076ab9ab67
Eghan-codes <eghanimma17@gmail.com>
2026-02-12 17:37:34 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
-import { menuArray } from "/data.js"
+import { menuArray } from "./data.js";
+
+const itemDisplay = document.getElementById('container')
+const ordersContainer = document.getElementById('orders')
+const orderList = document.getElementById('order-list')
+const orderTotal = document.getElementById('total')
 console.log(menuArray)
-
-
-function getMenuArr(menu){
-    const items = document.getElementById('menu-items')
-    let html = ``    
-    for(let item of menu){
-        html += `
-        <div class = "menu-content">
-        <div><h2>${item.emoji}</h2></div>
-                <div class="item-details">
-                    <div>
-                        <p class ='name'>${item.name}</p>
-                        <p class ='ingredient'>${item.ingredients}</p>
-                        <p class ='price'>¢${item.price}</p>
-                    </div>
-                    <button>+</button>
-                    </div>
-                
-        </div>
+let arrOfOrders = []
+// This function only displays our items/product to user in the web browser.
+function displayItems(){
+    let html = ``
+    menuArray.forEach(function(item){
+        html+= `
+            <div class='card-item'>
+                <span>${item.emoji}</span>
+                <div>
+                    <h2>${item.name}</h2>
+                    <p>${item.ingredients}</p>
+                    <p>$${item.price}</p>
+                </div>
+                <button data-item='${item.id}'>+</button>
+            </div>
         `
-    }
-    items.innerHTML = html
+    })
+    return itemDisplay.innerHTML = html
 }
-getMenuArr(menuArray)
+displayItems()
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.item){
+        handleAddItem(e.target.dataset.item)
+    }
+
+    if(e.target.dataset.remove){
+        handleRemoveItem(e.target.dataset.remove)
+    }
+})
+
+function handleAddItem(itemId){
+    let html = ``
+    arrOfOrders.push(menuArray[itemId])
+    arrOfOrders.forEach(function(item){
+        html += `
+            <ul>
+                <li>${item.name} <button data-remove=${item.id}>remove</button> <span>$${item.price}</span></li>
+            </ul>
+        `
+    })
+    ordersContainer.style.display = 'block'
+    console.log(arrOfOrders)
+    orderList.innerHTML = html
+
+    const prices = arrOfOrders.map(function(item){
+        return item.price
+    })
+
+    const totalPrice = prices.reduce(function(total, currentPrice){
+        return total + currentPrice
+    })
+    orderTotal.innerHTML = `<p>Total Price: $${totalPrice}</p>`
+}
+
+function handleRemoveItem(itemId){
+    arrOfOrders.pop(itemId)
+    console.log(arrOfOrders)
+}
